4b94100f108f92ba622b89faf69d91aec49366d0
Ceball0s <ceballosmiguel44@gmail.com>
2025-06-21 19:17:50 -0500

    Update bug

--- src/main/java/com/Oferta/OfertaApplication.java
+++ src/main/java/com/Oferta/OfertaApplication.java
@@ -2,8 +2,8 @@ package com.Oferta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
