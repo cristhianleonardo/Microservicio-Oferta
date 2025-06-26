6780ec5e41e098637d9a6a732de0b0a3ea348ce0
cristhianleonardo <cristhian.albarracin@correounivalle.edu.co>
2025-06-26 15:13:55 -0500

    fix

--- dockerfile
+++ dockerfile
@@ -2,10 +2,8 @@
 FROM maven:3.9.6-eclipse-temurin-21 AS build
 WORKDIR /app
 COPY . .
-COPY .env .env
-COPY .env /app/.env
 RUN mvn clean package -DskipTests
-COPY .env /app/.env
+RUN ls -l /app/target/
 
 # Etapa 2: empaquetar la app
 FROM eclipse-temurin:21-jdk-alpine
--- src/main/java/com/Oferta/OfertaController.java
+++ src/main/java/com/Oferta/OfertaController.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ofertas")
+@RequestMapping("/api/ofertas")
 @CrossOrigin(origins = {"http://localhost:5173"})
 @RequiredArgsConstructor
 public class OfertaController {
